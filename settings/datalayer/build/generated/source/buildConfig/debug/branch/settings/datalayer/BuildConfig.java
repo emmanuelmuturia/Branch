bc1c63ebe54347182caeacb03e561535755b89e7
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-02-08 23:53:59 +0300

    The Untracked Files...

---
+++ settings/datalayer/build/generated/source/buildConfig/debug/branch/settings/datalayer/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package branch.settings.datalayer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "branch.settings.datalayer";
+  public static final String BUILD_TYPE = "debug";
+  public static final String privacyPolicy = "https://branch.co.ke/pp";
+  public static final String termsOfUse = "https://branch.co.ke/tou/ke";
+}
