44298b604f011d381790b626d8c1f499ab13802e
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-02-08 15:53:07 +0300

    Set up Libraries and Modules...

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+h
+8app/src/main/java/emmanuelmuturia/branch/MainActivity.kt,2/3/23b5484050439fc1df2cb2a0341d33fb4b43334f
+D
+app/build.gradle.kts,d/2/d2a4383452aad84a18429a5844d31330a012a9e6
+I
+gradle/libs.versions.toml,d/7/d7595319a0658be6f1ed500ad590d42e4b28bc4f
+@
+build.gradle.kts,d/b/dbcff70658daf80b53ce624f6adcaa529df5ed8d
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,18 @@ dependencyResolutionManagement {
 
 rootProject.name = "Branch"
 include(":app")
- 
+include(":commons:uilayer")
+include(":commons:domainlayer")
+include(":commons:datalayer")
+include(":navigation")
+include(":network:datalayer")
+include(":network:domainlayer")
+include(":messages:uilayer")
+include(":messages:domainlayer")
+include(":messages:datalayer")
+include(":settings:uilayer")
+include(":settings:domainlayer")
+include(":settings:datalayer")
+include(":authentication:uilayer")
+include(":authentication:domainlayer")
+include(":authentication:datalayer")
